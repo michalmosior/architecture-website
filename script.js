9200c27aa0ad2e95736b5dd54a424ab64bf20155
michalmosior <michal.mosior@gmail.com>
2022-07-03 11:57:12 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const nav = document.querySelector('.nav__list');
 const burgerBtn = document.querySelector('.nav__burger-btn');
 
 const handleNav = () => {
-	const navItems = document.querySelectorAll('.nav__item');
+	burgerAnimation();
+	menuLinkAnimation();
+	nav.classList.toggle('nav__active');
+	document.body.classList.toggle('overflow-hidden');
+};
+
+const burgerAnimation = () => {
 	const burgerBar = document.querySelectorAll('.nav__burger-bar');
 	burgerBar[0].classList.toggle('nav__burger-bar--rotate-first');
 	burgerBar[1].classList.toggle('nav__burger-bar--hide');
 	burgerBar[2].classList.toggle('nav__burger-bar--rotate-second');
-	nav.classList.toggle('nav__active');
-	document.body.classList.toggle('overflow-hidden');
-	navItems.forEach((navItem) => {
-		navItem.classList.add('nav__active');
-	});
 };
 
+const menuLinkAnimation = () => {
+	const navItems = document.querySelectorAll('.nav__item');
+	navItems.forEach((navItem) => {
+		navItem.classList.toggle('nav__active');
+	});
+};
 burgerBtn.addEventListener('click', handleNav);
